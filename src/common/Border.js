857b49f05afefb0f3c66dbcf2dee61fa31449b0f
colorski <290518066@qq.com>
2018-08-22 14:27:47 +0800

    update common Header and Border

---
+++ src/common/Border.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './Border.css'
+
+export default class Border extends Component {
+	render(){
+		return (
+			<div className="App-border"><p></p><p></p><p></p><p></p><p></p></div>
+		)
+	}
+}
