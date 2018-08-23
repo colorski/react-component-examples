c2eb1253090e221fc7721c0678820c153fa8d7d1
colorski <290518066@qq.com>
2018-08-23 10:44:34 +0800

    list

--- src/common/List.js
+++ src/common/List.js
@@ -1,41 +1,53 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './List.css';
 
 export default class List extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      list: []
+    }
+  }
+
   render () {
     return (
       <div className="list-wrapper">
-        <ul>
-          <li>
-            <h3>标题</h3>
-            <p>描述文字</p>
-          </li>
-          <li>
-            <h3>标题</h3>
-            <p>描述文字</p>
-          </li>
-        </ul>
-        <ul>
-          <li>
-            <h3>标题</h3>
-            <p>描述文字</p>
-          </li>
-          <li>
-            <h3>标题</h3>
-            <p>描述文字</p>
-          </li>
-        </ul>
-        <ul>
-          <li>
-            <h3>标题</h3>
-            <p>描述文字</p>
-          </li>
-          <li>
-            <h3>标题</h3>
-            <p>描述文字</p>
-          </li>
-        </ul>
+        <ul>{ this.initListData() }</ul>
+        <ul>{ this.initListData() }</ul>
+        <ul>{ this.initListData() }</ul>
       </div>
     )
+  }
+
+  initListData () {
+    const { list } = this.state;
+    if(list.length){
+      return (
+        list.map((item) => (
+          <li key={item.id}>
+            <h3>{item.title}</h3>
+            <p>{item.desc}</p>
+          </li>
+        ))
+      )
+    }else{
+      return (
+        <p className="no-tips">暂无内容！</p>
+      )
+    }
+  }
+
+  componentDidMount () {
+    axios.get('/api/list')
+      .then((res) => 
+        {
+          const data = [...res.data];
+          this.setState({
+            list: data
+          })
+        }
+      )
+      .catch((err) => console.log(err))
   }
 }
